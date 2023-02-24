4320ef61ca4f7334e9e0101eb9f1ba9238a46563
s-kakol <kakolsylwester@gmail.com>
2023-02-24 14:47:15 +0100

    Rebase to DEV-70, remove users\*.py module

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django_extensions.db.models import TimeStampedModel
-
-
-class User(TimeStampedModel):
-    email = models.EmailField('email address', unique=True)
-    username = models.CharField(max_length=64, unique=True)
-    password_hash = models.CharField(max_length=255)
