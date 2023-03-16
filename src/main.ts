54f1e94248040ef5c20a0636b9ebc8326d808b38
Sylwester Kąkol <39565249+s-kakol@users.noreply.github.com>
2023-03-16 20:19:28 +0100

    DEV-80 Add swagger config
    Configure basic documentation module.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig, swaggerOptions } from './utils/swagger.config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const config = swaggerConfig();
+  const swaggerDocument = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, swaggerDocument, swaggerOptions);
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
+
 bootstrap();
