6a2bbd60a1a8219085bbcc56a8b63cec4d95c22d
s-kakol <kakolsylwester@gmail.com>
2023-04-18 11:54:18 +0200

    DEV-112 Donation Module Unit Tests

---
+++ src/donation/donation.controller.spec.ts
@@ -0,0 +1,69 @@
+import { DonationController } from './donation.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { DonationService } from './donation.service';
+import * as fc from 'fast-check';
+
+describe('DonationController', () => {
+  const exampleDonation = {
+    user_id: 720,
+    donated_type: 'whole',
+    amount: 450,
+    blood_pressure: '170/90',
+    donated_at: '2002-02-02T22:22:22.22Z',
+  };
+  const exampleId = 1;
+
+  let controller: DonationController;
+
+  const mockDonationService = {
+    findDonationById: jest.fn(id => {
+      return {
+        id,
+        ...exampleDonation,
+      };
+    }),
+    createDonation: jest.fn(dto => {
+      return {
+        id: fc.integer(),
+        ...dto,
+      };
+    }),
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
+    removeDonation: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [DonationController],
+      providers: [DonationService],
+    })
+      .overrideProvider(DonationService)
+      .useValue(mockDonationService)
+      .compile();
+
+    controller = module.get<DonationController>(DonationController);
+  });
+
+  it('Should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('Finds a donation', () => {
+    expect(controller.getDonation(exampleId)).toEqual({
+      id: exampleId,
+      ...exampleDonation,
+    });
+  });
+
+  it('Creates a donation', () => {
+    expect(controller.addDonation(exampleDonation)).resolves.toEqual({
+      id: fc.integer(),
+      ...exampleDonation,
+    });
+  });
+
+  it('Should delete a donation', async () => {
+    await expect(controller.deleteDonation(exampleId)).resolves.not.toThrow();
+    expect(mockDonationService.removeDonation).toHaveBeenCalledWith(exampleId);
+  });
+});
---
+++ src/donation/donation.service.spec.ts
@@ -0,0 +1,85 @@
+import { DonationService } from './donation.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DonationEntity } from './models/donation.entity';
+import { UserService } from '../user/user.service';
+
+describe('DonationService', () => {
+  const exampleDonation = {
+    user_id: 720,
+    donated_type: 'whole',
+    amount: 450,
+    blood_pressure: '170/90',
+    donated_at: '2002-02-02T22:22:22.22Z',
+  };
+  const exampleId = 1;
+
+  let service: DonationService;
+
+  const mockUserService = {
+    findUserById: jest.fn(id => id),
+  };
+  const mockDonationRepository = {
+    findOneBy: jest.fn().mockImplementation(id => {
+      return {
+        ...id,
+        ...exampleDonation,
+      };
+    }),
+    create: jest.fn().mockImplementation(dto => {
+      return {
+        id: exampleId,
+        ...dto,
+      };
+    }),
+    save: jest.fn().mockImplementation(dto => dto),
+    softDelete: jest.fn().mockImplementation(() => {
+      return { affected: 1 };
+    }),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        DonationService,
+        UserService,
+        {
+          provide: getRepositoryToken(DonationEntity),
+          useValue: mockDonationRepository,
+        },
+      ],
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
+
+    service = module.get<DonationService>(DonationService);
+  });
+
+  it('Should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('Should find donation by id', () => {
+    expect(service.findDonationById(exampleId)).resolves.toEqual({
+      id: exampleId,
+      ...exampleDonation,
+    });
+  });
+
+  it('Should create donation', () => {
+    expect(service.createDonation(exampleDonation)).resolves.toEqual({
+      id: exampleId,
+      user: 720,
+      donated_type: 'whole',
+      amount: 450,
+      blood_pressure: '170/90',
+      donated_at: '2002-02-02T22:22:22.22Z',
+    });
+  });
+
+  it('Should remove donation', async () => {
+    await expect(service.removeDonation(exampleId)).resolves.not.toThrow();
+    expect(mockDonationRepository.softDelete).toHaveBeenCalledWith(exampleId);
+  });
+});
